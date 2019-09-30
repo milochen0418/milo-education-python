cd45d800f2a44e0e3ac092e6ec7cbedf1d47d410
Milo Chen <milo_chen@mail2000.com.tw>
2019-09-30 11:12:36 +0800

    add reset_all_3dcursor.py

---
+++ Blender/reset_all_3dcursor.py
@@ -0,0 +1,5 @@
+view3ds = [area.spaces[0] for area in bpy.context.screen.areas if (area.type=="VIEW_3D")]
+for view3d in view3ds:
+    view3d.pivot_point='CURSOR'
+    view3d.cursor_location = (0.0, 0.0, 0.0)
+
